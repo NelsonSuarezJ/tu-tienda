6dcb0e224e00aa609f2815ffc48416c8203826a0
Nelson <nelsonsuarezj@gmail.com>
2021-11-05 20:48:22 -0500

    se crea componente cards de inicio

--- src/components/CardProductos.jsx
+++ src/components/CardProductos.jsx
@@ -3,7 +3,7 @@ import { Card, Button } from 'react-bootstrap'
 
 const CardProductos = () => {
     return (
-        <Card className="mt-3 shadow w-75 mx-auto">
+        <Card className="mt-4 shadow w-75 mx-auto">
             <Card.Img variant="top" src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1633319763/Tu%20Tienda%20Cercana/productos_zqgusu.png" />
             <Card.Body className="d-grid">
                 <Button variant="warning" size="">IR</Button>
