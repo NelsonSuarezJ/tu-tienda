045a1fcbf5f2693c088118ce16e3aee39943fd94
Andrea Jaramillo <andrepijg@gmail.com>
2021-11-11 00:58:31 -0500

    formulario login no vinculado

--- src/components/Cards/CardProductos.jsx
+++ src/components/Cards/CardProductos.jsx
@@ -5,15 +5,16 @@ import "./CardProductos.css"
 const CardProductos = () => {
     return (
     <Container>
+    <h1>Nuestros Productos </h1>
         <Row>
             <Col xs={6} md={4}>
-                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"/>
+                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png" width="300"/>
             </Col>
             <Col xs={6} md={4}>
-                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png" />
+                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png" width="300"/>
             </Col>
             <Col xs={6} md={4}>
-                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png" rounded />
+                <Image src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png" width="300" />
             </Col>
         </Row>
     </Container>
