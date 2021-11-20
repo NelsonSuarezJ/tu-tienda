03f76bbc5089d8ba77620ed3fcd7357ed8e9f37d
Andrea Jaramillo <andrepijg@gmail.com>
2021-11-19 20:51:04 -0500

    Andrea.js

---
+++ src/andrea.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class ProductosNuevos  extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            console.log("hola ")
+        );
+    }
+}
+
