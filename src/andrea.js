0a4db46238e66c8282666ecc771cea62bb187f98
Carlos Jaramillo <cajaramillob@gmail.com>
2021-11-19 21:39:13 -0500

    Blog

--- src/andrea.js
+++ src/andrea.js
@@ -1,19 +1,16 @@
-import React from 'react';
-
+import React from "react";
 
 export default class ProductosNuevos extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div>
-                console.log("hola ");
-                console.log("Hola mundo");
-                console.log("archivo modificado al tiempo")
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        console.log("hola "); console.log("Hola mundo"); console.log("archivo
+        modificado al tiempo") console.log("blog por Carlos")
+      </div>
+    );
+  }
 }
-
