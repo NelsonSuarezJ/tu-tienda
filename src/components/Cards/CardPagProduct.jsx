6c05a0f68e5e6d7f9113d4a1772e7c604ddebeb7
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-11-21 16:28:30 -0500

    terminadoel buscador

--- src/components/Cards/CardPagProduct.jsx
+++ src/components/Cards/CardPagProduct.jsx
@@ -1,33 +1,460 @@
-import React from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
-import fruta from '../../assets/mango.png'
+import React, { useState, useEffect, useMemo } from "react";
+import { Card, Container } from "react-bootstrap";
+// import fruta from "../../assets/mango.png";
+import "./CardProductos.css";
 
 const CardPagProduct = () => {
-    return (
-        <Container>
-            <h1 className="text-center my-5">PRODUCTOS</h1>
-            <Row>
-                <Col xs={3}>
-                    <Card>
-                        <Card.Img variant="top" src={fruta} width="10px" />
-                        <Card.Body>
-                            <Card.Title>Mango</Card.Title>
-                            <Card.Subtitle className="mb-4 text-muted">Frutas</Card.Subtitle>
-                            <Card.Text>
-                                Rapitienda El tesoro
-                            </Card.Text>
-                            <hr />
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-                            </svg>
-                            <span className="mx-2">3001234567</span>
-                            <h3 className="mt-2">$ 6200</h3>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+  const productos = [
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d726",
+      },
+      nombre_tienda: "Tienda Bienaventurada",
+      telefono_tienda: 3205148963,
+      email_tienda: "tiendabienaventurada@gmail.com",
+      direccion_tienda: "Cra. 36 # 34-03 ",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Banano",
+      precio_kg: 1400,
+      url_img: "https://bit.ly/Banano_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d727",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Banano",
+      precio_kg: 1300,
+      url_img: "https://bit.ly/Banano_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d728",
+      },
+      nombre_tienda: "Tienda la Cosecha",
+      telefono_tienda: 3019857845,
+      email_tienda: "lacosecha@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 32 Cs 7",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Frutas",
+      nombre_producto: "Coco",
+      precio_kg: 2800,
+      url_img: "https://bit.ly/Coco_coco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d729",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Coco",
+      precio_kg: 3000,
+      url_img: "https://bit.ly/Coco_coco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72a",
+      },
+      nombre_tienda: "Tienda Angelita",
+      telefono_tienda: 3156983564,
+      email_tienda: "tiendangelita@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 50 Cs 25",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Frutas",
+      nombre_producto: "Mango",
+      precio_kg: 1200,
+      url_img: "https://bit.ly/Mango_oneco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72b",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Mango",
+      precio_kg: 1300,
+      url_img: "https://bit.ly/Mango_oneco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72c",
+      },
+      nombre_tienda: "Tienda la Moneda",
+      telefono_tienda: 3016938745,
+      email_tienda: "lamoneda@gmail.com",
+      direccion_tienda: "Manzana 29 Local 1, Bosques de la Acuarela",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Frutas",
+      nombre_producto: "Mango",
+      precio_kg: 1400,
+      url_img: "https://bit.ly/Mango_oneco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72d",
+      },
+      nombre_tienda: "Rapitienda El tesoro",
+      telefono_tienda: 3226589632,
+      email_tienda: "rapieltesoro@gmail.com",
+      direccion_tienda: "Carrera 50 #49-29",
+      ciudad: "Andes",
+      tipo_producto: "Frutas",
+      nombre_producto: "Mango",
+      precio_kg: 1500,
+      url_img: "https://bit.ly/Mango_oneco",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72e",
+      },
+      nombre_tienda: "Tienda Angelita",
+      telefono_tienda: 3156983564,
+      email_tienda: "tiendangelita@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 50 Cs 25",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Frutas",
+      nombre_producto: "Manzana Roja",
+      precio_kg: 6400,
+      url_img: "https://bit.ly/Manzana_Roja",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d72f",
+      },
+      nombre_tienda: "Rapitienda El tesoro",
+      telefono_tienda: 3226589632,
+      email_tienda: "rapieltesoro@gmail.com",
+      direccion_tienda: "Carrera 50 #49-29",
+      ciudad: "Andes",
+      tipo_producto: "Frutas",
+      nombre_producto: "Manzana Roja",
+      precio_kg: 6500,
+      url_img: "https://bit.ly/Manzana_Roja",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d730",
+      },
+      nombre_tienda: "Tienda Bienaventurada",
+      telefono_tienda: 3205148963,
+      email_tienda: "tiendabienaventurada@gmail.com",
+      direccion_tienda: "Cra. 36 # 34-03 ",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Manzana Roja",
+      precio_kg: 6300,
+      url_img: "https://bit.ly/Manzana_Roja",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d731",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Frutas",
+      nombre_producto: "Kiwi",
+      precio_kg: 7500,
+      url_img: "https://bit.ly/Kiwi_Kiwi",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d732",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Ajo",
+      precio_kg: 6300,
+      url_img: "https://bit.ly/Ajo_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d733",
+      },
+      nombre_tienda: "Tienda Angelita",
+      telefono_tienda: 3156983564,
+      email_tienda: "tiendangelita@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 50 Cs 25",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Verduras",
+      nombre_producto: "Ajo",
+      precio_kg: 6200,
+      url_img: "https://bit.ly/Ajo_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d734",
+      },
+      nombre_tienda: "Tienda la Cosecha",
+      telefono_tienda: 3019857845,
+      email_tienda: "lacosecha@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 32 Cs 7",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Verduras",
+      nombre_producto: "Ajo",
+      precio_kg: 6100,
+      url_img: "https://bit.ly/Ajo_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d735",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Pepino",
+      precio_kg: 800,
+      url_img: "https://bit.ly/Pepino_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d736",
+      },
+      nombre_tienda: "Rapitienda El tesoro",
+      telefono_tienda: 3226589632,
+      email_tienda: "rapieltesoro@gmail.com",
+      direccion_tienda: "Carrera 50 #49-29",
+      ciudad: "Andes",
+      tipo_producto: "Verduras",
+      nombre_producto: "Pepino",
+      precio_kg: 900,
+      url_img: "https://bit.ly/Pepino_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d737",
+      },
+      nombre_tienda: "Tienda Bienaventurada",
+      telefono_tienda: 3205148963,
+      email_tienda: "tiendabienaventurada@gmail.com",
+      direccion_tienda: "Cra. 36 # 34-03 ",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Pepino",
+      precio_kg: 850,
+      url_img: "https://bit.ly/Pepino_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d738",
+      },
+      nombre_tienda: "Tienda Bienaventurada",
+      telefono_tienda: 3205148963,
+      email_tienda: "tiendabienaventurada@gmail.com",
+      direccion_tienda: "Cra. 36 # 34-03 ",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Tomate",
+      precio_kg: 1500,
+      url_img: "https://bit.ly/Tomate_Tomate",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d739",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Tomate",
+      precio_kg: 1600,
+      url_img: "https://bit.ly/Tomate_Tomate",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73a",
+      },
+      nombre_tienda: "Tienda Angelita",
+      telefono_tienda: 3156983564,
+      email_tienda: "tiendangelita@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 50 Cs 25",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Verduras",
+      nombre_producto: "Tomate",
+      precio_kg: 1550,
+      url_img: "https://bit.ly/Tomate_Tomate",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73b",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Verduras",
+      nombre_producto: "Zanahoria",
+      precio_kg: 1900,
+      url_img: "https://bit.ly/Zanahoria_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73c",
+      },
+      nombre_tienda: "Tienda la Moneda",
+      telefono_tienda: 3016938745,
+      email_tienda: "lamoneda@gmail.com",
+      direccion_tienda: "Manzana 29 Local 1, Bosques de la Acuarela",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Verduras",
+      nombre_producto: "Zanahoria",
+      precio_kg: 1950,
+      url_img: "https://bit.ly/Zanahoria_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73d",
+      },
+      nombre_tienda: "Tienda la Cosecha",
+      telefono_tienda: 3019857845,
+      email_tienda: "lacosecha@gmail.com",
+      direccion_tienda: "Santa Isabel Mz 32 Cs 7",
+      ciudad: "Dosquebradas",
+      tipo_producto: "Verduras",
+      nombre_producto: "Zanahoria",
+      precio_kg: 2050,
+      url_img: "https://bit.ly/Zanahoria_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73e",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Carnes",
+      nombre_producto: "Lomo",
+      precio_kg: 28000,
+      url_img: "https://bit.ly/Lomo_res",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d73f",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Carnes",
+      nombre_producto: "Sobrebarriga",
+      precio_kg: 20000,
+      url_img: "https://bit.ly/Sobrebarriga_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d740",
+      },
+      nombre_tienda: "Rapitienda El tesoro",
+      telefono_tienda: 3226589632,
+      email_tienda: "rapieltesoro@gmail.com",
+      direccion_tienda: "Carrera 50 #49-29",
+      ciudad: "Andes",
+      tipo_producto: "Carnes",
+      nombre_producto: "Lomo",
+      precio_kg: 30000,
+      url_img: "https://bit.ly/Lomo_res",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d741",
+      },
+      nombre_tienda: "Rapitienda JP",
+      telefono_tienda: 3185470364,
+      email_tienda: "rapitiendajp@gmail.com",
+      direccion_tienda: "Carrera 36 # 34-03",
+      ciudad: "Pereira",
+      tipo_producto: "Carnes",
+      nombre_producto: "Pechuga",
+      precio_kg: 15000,
+      url_img: "https://bit.ly/Pechuga_",
+    },
+    {
+      _id: {
+        $oid: "618dca0bca600d86f5e4d742",
+      },
+      nombre_tienda: "Tienda Bienaventurada",
+      telefono_tienda: 3205148963,
+      email_tienda: "tiendabienaventurada@gmail.com",
+      direccion_tienda: "Cra. 36 # 34-03 ",
+      ciudad: "Pereira",
+      tipo_producto: "Carnes",
+      nombre_producto: "Pechuga",
+      precio_kg: 18000,
+      url_img: "https://bit.ly/Pechuga_",
+    },
+  ];
+  const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    setProduct(productos);
+  }, []);
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+  const filteredProduct = useMemo(
+    () =>
+      product.filter((product) => {
+        return product.nombre_producto
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      }),
+    [product, search]
+  );
 
-export default CardPagProduct
+  return (
+    <Container>
+      <h1 className="text-center my-5">PRODUCTOS</h1>
+      <div className="search">
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
+      <div className="container-products">
+        {filteredProduct.map((product) => (
+          <Card className="container-cards">
+            <Card.Img variant="top" src={product.url_img} width="10px" />
+            <Card.Body>
+              <Card.Title>{product.nombre_producto}</Card.Title>
+              <Card.Subtitle className="mb-4 text-muted">
+                {product.tipo_producto}
+              </Card.Subtitle>
+              <Card.Text>{product.nombre_tienda}</Card.Text>
+              <hr />
+              <span className="mx-2">{product.telefono_tienda}</span>
+              <h3 className="mt-2">$ {product.precio_kg}</h3>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default CardPagProduct;
--- src/components/Cards/CardProductos.jsx
+++ src/components/Cards/CardProductos.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import "./CardProductos.css";
+// import "./CardProductos.css";
 
 const CardProductos = () => {
-    return (
-        <Container className="container-cards-productos">
-            <div className="background-ola">
-                <h1>Nuestros Productos </h1>
-                <div className="container-links-cards">
-                    <a href="https://www.youtube.com" className="imagen-link">
-                        <img
-                            src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
-                            alt=""
-                        />
-                    </a>
-                    <a href="https://www.youtube.com" className="imagen-link">
-                        <img
-                            src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
-                            alt=""
-                        />
-                    </a>
-                    <a href="https://www.youtube.com" className="imagen-link">
-                        <img
-                            src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
-                            alt=""
-                        />
-                    </a>
-                </div>
-            </div>
-            <div className="wave"></div>
-        </Container>
-    );
+  return (
+    <Container className="container-cards-productos">
+      <div className="background-ola">
+        <h1>Nuestros Productos </h1>
+        <div className="container-links-cards">
+          <a href="https://www.youtube.com" className="imagen-link">
+            <img
+              src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
+              alt=""
+            />
+          </a>
+          <a href="https://www.youtube.com" className="imagen-link">
+            <img
+              src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
+              alt=""
+            />
+          </a>
+          <a href="https://www.youtube.com" className="imagen-link">
+            <img
+              src="https://res.cloudinary.com/dgjrfgl2e/image/upload/v1636605665/Tu%20Tienda%20Cercana/VERDURAS_zcl0ma.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div className="wave"></div>
+    </Container>
+  );
 };
 
 export default CardProductos;
